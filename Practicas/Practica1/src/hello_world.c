acb353525e88a572e1ffce95bd19be4c95fd7d75
Marcos Dominguez <mrds0690@gmail.com>
2024-03-04 21:41:28 -0300

    feat: code fit to linux coding rules

--- Practicas/Practica1/src/hello_world.c
+++ Practicas/Practica1/src/hello_world.c
@@ -1,15 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/utsname.h>
 
-static int __init hello_init(void) {
-    printk(KERN_INFO "Hola Mundo. Estás utilizando Linux %s\n", init_uts_ns.name.release);
-    return 0; 
+static int __init hello_init(void)
+{
+	pr_info("Hola Mundo. Estás utilizando Linux %s\n", init_uts_ns.name.release);
+	return 0;
 }
 
-static void __exit hello_exit(void) {
-    printk(KERN_INFO "Adiós Mundo.\n");
+static void __exit hello_exit(void)
+{
+	pr_info("Adiós Mundo.\n");
 }
 
 module_init(hello_init);
