84ea6ed0c057723950afc9d496de98ebfb3e7744
Marcos Dominguez <mrds0690@gmail.com>
2024-04-15 21:09:16 -0300

    feat: add checks and fix memcpy to copy from/to

--- Practicas/Entregas/TPFinal/I2C-Drivers/ads111x_driver.c
+++ Practicas/Entregas/TPFinal/I2C-Drivers/ads111x_driver.c
@@ -3,14 +3,14 @@
 #include <linux/i2c.h>
 #include <linux/fs.h>
 #include <linux/of.h>
-#include <linux/string.h>
+#include <linux/uaccess.h>
 
 /*========= [PRIVATE DEVICE STRUCTURE] =========================================*/
 
 struct ads111x_dev {
-	struct i2c_client *client;
-	struct miscdevice ads111x_misc_device;
-	char name[9]; /* ads111xXX */
+    struct i2c_client *client;
+    struct miscdevice ads111x_misc_device;
+    char name[10]; /* ads111xXX */
 };
 
 /*========= [DEVICE TREE IDENTIFIERS] ==========================================*/
@@ -19,7 +19,6 @@ static const struct of_device_id ads111x_dt_ids[] = {
     { .compatible = "mse,ads111x", },
     { /* sentinel */ }
 };
-
 MODULE_DEVICE_TABLE(of, ads111x_dt_ids);
 
 /*========= [I2C HELPER FUNCTIONS] =============================================*/
@@ -42,53 +41,80 @@ static int i2c_read_reg(struct i2c_client *client, uint8_t reg, uint8_t *data, s
     return i2c_transfer(client->adapter, msgs, 2);
 }
 
-static int i2c_write_reg(struct i2c_client *client, const char *data, size_t len) {
-    struct i2c_msg msgs[1];
-    static char buff_to_send[3];
+static int i2c_write_reg(struct i2c_client *client, char *data, size_t len) {
+    struct i2c_msg msg;
+    
     if (len > 3) {
-        return -1;
-    } else {
-        memcpy(buff_to_send, data, len);
-        msgs[0].addr = client->addr;
-        msgs[0].flags = 0;
-        msgs[0].buf = buff_to_send;
-        msgs[0].len = len;
-
-        return i2c_transfer(client->adapter, msgs, 1);
+        pr_err("Data length greater than buffer size\n");
+        return -EINVAL;
     }
+    
+    msg.addr = client->addr;
+    msg.flags = 0;
+    msg.buf = data;
+    msg.len = len;
+    
+    return i2c_transfer(client->adapter, &msg, 1);
 }
 
 /*========= [FILE OPERATIONS] ===================================================*/
 
 static ssize_t ads111x_read(struct file *file, char __user *userbuf, size_t len, loff_t *ppos) {
     struct ads111x_dev *ads111x;
-    int ret = 0;
-    uint8_t reg = userbuf[0];
+    uint8_t reg;
     uint8_t data[2];
-    if (len > 2) {
-        pr_err("Requested register size greater than 2");
-    } else {
-        ads111x = container_of(file->private_data, struct ads111x_dev, ads111x_misc_device);
-        ret = i2c_read_reg(ads111x->client, reg, data, len);
-        if (ret < 0) {
-            pr_err("Error reading register: %d\n", ret);
-        } else {
-            memcpy(userbuf, data, len);
-        }
+    int ret;
+    
+    if (len != sizeof(data)) {
+        pr_err("Invalid read size\n");
+        return -EINVAL;
     }
-    return ret;
+
+    if (copy_from_user(&reg, userbuf, sizeof(reg)) != 0) {
+        pr_err("Failed to copy register from user space\n");
+        return -EFAULT;
+    }
+
+    ads111x = container_of(file->private_data, struct ads111x_dev, ads111x_misc_device);
+    ret = i2c_read_reg(ads111x->client, reg, data, sizeof(data));
+    if (ret < 0) {
+        pr_err("Error reading register: %d\n", ret);
+        return ret;
+    }
+
+    if (copy_to_user(userbuf, data, sizeof(data)) != 0) {
+        pr_err("Failed to copy data to user space\n");
+        return -EFAULT;
+    }
+
+    return sizeof(data);
 }
 
-static ssize_t ads111x_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset) {
+static ssize_t ads111x_write(struct file *file, const char __user *userbuf, size_t len, loff_t *offset) {
     struct ads111x_dev *ads111x;
-    int ret = 0;
+    char buf[3];
+    int ret;
+
+    if (len > sizeof(buf)) {
+        pr_err("Data length greater than buffer size\n");
+        return -EINVAL;
+    }
+
+    if (copy_from_user(buf, userbuf, len) != 0) {
+        pr_err("Failed to copy data from user space\n");
+        return -EFAULT;
+    }
+
     ads111x = container_of(file->private_data, struct ads111x_dev, ads111x_misc_device);
-    ret = i2c_write_reg(ads111x->client, buffer, len);
+    ret = i2c_write_reg(ads111x->client, buf, len);
     if (ret < 0) {
         pr_err("Error writing register: %d\n", ret);
+        return ret;
     }
-    return ret;
+
+    return len;
 }
+
 
 static long ads111x_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     struct ads111x_dev *ads111x;
@@ -116,10 +142,13 @@ static int ads111x_probe(struct i2c_client *client, const struct i2c_device_id *
     int ret_val;
 
     ads111x = devm_kzalloc(&client->dev, sizeof(struct ads111x_dev), GFP_KERNEL);
+    if (!ads111x)
+        return -ENOMEM;
+        
     i2c_set_clientdata(client, ads111x);
     ads111x->client = client;
 
-    sprintf(ads111x->name, "ads111x%02d", counter++);
+    snprintf(ads111x->name, sizeof(ads111x->name), "ads111x%02d", counter++);
 
     ads111x->ads111x_misc_device.name = ads111x->name;
     ads111x->ads111x_misc_device.minor = MISC_DYNAMIC_MINOR;
